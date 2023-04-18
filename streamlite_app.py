b2a3e343f69aa73949d3d715bb0e5032b5fa4c63
Sofía Brenda Schneider <112521839+sofiaSchneider1@users.noreply.github.com>
2023-04-18 12:04:30 -0300

    Update streamlite_app.py
    added a header

--- streamlite_app.py
+++ streamlite_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣Omega 3 & Blubeberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
