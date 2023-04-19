f2915aaa935b45dd9331ed80e90bddde38ce7972
Sofía Brenda Schneider <112521839+sofiaSchneider1@users.noreply.github.com>
2023-04-19 16:17:57 -0300

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -33,9 +33,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-//
+
+#here it is the stop
 streamlit.stop()
-//
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
